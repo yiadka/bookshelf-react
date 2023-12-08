ccf3c0050b45787d5b03a716f6911edcaa754b99
yiadka <moondrip4869@gmail.com>
2023-12-09 01:47:47 +0900

    kakezan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1>Count: {count}</h1>
+      <button 
+        onClick={() => setCount(count + 1)}
+      >
+        increment
+      </button>
+      <button 
+        onClick={() => setCount(count - 1)}
+      >
+        decrement
+      </button>
+      <button 
+        onClick={() => setCount(count * 2)}
+      >
+        kakezan
+      </button>
     </div>
+    </>
   );
 }
-
-export default App;
